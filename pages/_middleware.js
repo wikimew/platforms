10821d21db5450cdb98e5e4cbb21cc6f0124ce11
wikimew <wikili2003@yandex.com>
2022-01-22 07:52:18 +0000

    update domain

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,14 +4,9 @@ export default function middleware(req) {
   const { pathname } = req.nextUrl; // get pathname of request (e.g. /blog-slug)
   const hostname = req.headers.get("host"); // get hostname of request (e.g. demo.vercel.pub)
 
-  if (hostname === "vercel.pub") {
-    // only for demo purposes – remove this if you want to use your root domain as the landing page
-    return NextResponse.redirect("https://demo.vercel.pub");
-  }
-
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname.replace(`.vercel.pub`, "")
+      ? hostname.replace(`.eggheadexperts.gq`, "")
       : hostname.replace(`.localhost:3000`, "");
 
   if (pathname.startsWith(`/_sites`)) {
